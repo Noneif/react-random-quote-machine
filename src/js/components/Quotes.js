4432e3d18f5aaccbeccc59fee95db85d7791be2c
NoName <sofar0222@gmail.com>
2023-04-26 03:06:31 +0200

    add: Quotes.js

---
+++ src/js/components/Quotes.js
@@ -0,0 +1,279 @@
+export const Quotes = [
+  {
+    quote:
+      "To live is the rarest thing in the world. Most people exist, that is all.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote: "That it will never come again is what makes life so sweet.",
+    author: "Emily Dickinson",
+  },
+  {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot",
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    quote: "Pain is inevitable. Suffering is optional.",
+    author: "Haruki Murakami",
+  },
+  {
+    quote: "All the world's a stage, and all the men and women merely players.",
+    author: "William Shakespeare",
+  },
+  {
+    quote: "Be kind, for everyone you meet is fighting a hard battle.",
+    author: "Plato",
+  },
+  {
+    quote: "Unable are the loved to die for love is immortality.",
+    author: "Emily Dickinson",
+  },
+  {
+    quote: "Let me live, love, and say it well in good sentences.",
+    author: "Sylvia Plath",
+  },
+  {
+    quote: "Don't let your happiness depend on something you may lose.",
+    author: "C.S. Lewis",
+  },
+  {
+    quote: "We are all broken, that's how the light gets in.",
+    author: "Ernest Hemingway",
+  },
+  {
+    quote:
+      "Appreciation is a wonderful thing. It makes what is excellent in others belong to us as well.",
+    author: "Voltaire",
+  },
+  {
+    quote: "Life is tough my darling, but so are you.",
+    author: "Stephanie Bennett Henry",
+  },
+  {
+    quote: "Self-awareness and self-love matter. Who we are is how we lead.",
+    author: "Brene Brown",
+  },
+  {
+    quote:
+      "Amateurs sit and wait for inspiration, the rest of us just get up and go to work.",
+    author: "Stephen King",
+  },
+  {
+    quote:
+      "Get it down. Take chances. It may be bad, but it's the only way you can do anything really good.",
+    author: "William Faulkner",
+  },
+  {
+    quote: "As a writer, you should not judge, you should understand.",
+    author: "Ernest Hemingway",
+  },
+  {
+    quote: "To produce a mighty book, you must choose a mighty theme.",
+    author: "Herman Melville",
+  },
+  {
+    quote:
+      "Ideas are like rabbits. You get a couple and learn how to handle them, and pretty soon you have a dozen.",
+    author: "John Steinbeck",
+  },
+  {
+    quote: "I am not a failure, I am a life.",
+    author: "George Bernard Shaw",
+  },
+  {
+    quote:
+      "Sometimes, you read a book and it fills you with this weird evangelical zeal, and you become convinced that the shattered world will never be put back together unless and until all living humans read the book.",
+    author: "John Green",
+  },
+  {
+    quote:
+      "The Six Golden Rules of Writing: Read, read, read, and write, write, write.",
+    author: "Ernest Gaines",
+  },
+  {
+    quote: "Tears are words that need to be written.'",
+    author: "Paulo Coelho",
+  },
+  {
+    quote: "You can always edit a bad page. You can't edit a blank page.",
+    author: "Jodi Picoult",
+  },
+  {
+    quote: "You cannot find peace by avoiding life.",
+    author: "Virginia Woolf",
+  },
+  {
+    quote: "The strongest principle of growth lies in the human choice.",
+    author: "George Eliot",
+  },
+  {
+    quote:
+      "Focus more on your desire than on your doubt, and the dream will take care of itself.",
+    author: "Mark Twain",
+  },
+  {
+    quote:
+      "We have to continually be jumping off cliffs and developing our wings on the way down.",
+    author: "Kurt Vonnegut",
+  },
+  {
+    quote:
+      "I hope that in this year to come, you make mistakes. Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're doing something.",
+    author: "Neil Gaiman",
+  },
+  {
+    quote:
+      "Don't bend; don't water it down; don't try to make it logical; don't edit your own soul according to the fashion. Rather, follow your most intense obsessions mercilessly.",
+    author: "Franz Kafka",
+  },
+  {
+    quote:
+      "Keep away from people who try to belittle your ambitions. Small people always do that, but the really great make you feel that you, too, can become great.",
+    author: "Mark Twain",
+  },
+  {
+    quote:
+      "Maybe it's not about having a beautiful day, but about finding beautiful moments. Maybe a whole day is just too much to ask. I could choose to believe that in every day, in all things, no matter how dark and ugly, there are shards of beauty if I look for them.",
+    author: "Anna White",
+  },
+  {
+    quote:
+      "Trust our heart if the seas catch fire, live by love though the stars walk backwards.",
+    author: "E. E. Cummings",
+  },
+  {
+    quote: "One day I will find the right words, and they will be simple.",
+    author: "Jack Kerouac",
+  },
+  {
+    quote:
+      "I can be changed by what happens to me. But I refuse to be reduced by it.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "The most common way people give up their power is by thinking they don't have any.",
+    author: "Alice Walker",
+  },
+  {
+    quote:
+      "I want to taste and glory in each day, and never be afraid to experience pain.",
+    author: "Sylvia Plath",
+  },
+  {
+    quote: "If I waited for perfection, I would never write a word.",
+    author: "Margaret Atwood",
+  },
+  {
+    quote:
+      "How wonderful it is that nobody need wait a single moment before starting to improve the world.",
+    author: "Anne Frank",
+  },
+  {
+    quote:
+      "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Aristotle",
+  },
+  {
+    quote: "If you can dream it, you can achieve it.",
+    author: "Zig Ziglar",
+  },
+  {
+    quote: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale",
+  },
+  {
+    quote: "If the wind will not serve, take to the oars.",
+    author: "Latin Proverb",
+  },
+  {
+    quote:
+      "Happiness is not something readymade. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    quote: "You become what you believe.",
+    author: "Oprah Winfrey",
+  },
+  {
+    quote: "Remember no one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote:
+      "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    author: "Robert Frost",
+  },
+  {
+    quote:
+      "Go confidently in the direction of your dreams. Live the life you have imagined.",
+    author: "Henry David Thoreau",
+  },
+  {
+    quote:
+      "There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",
+    author: "Aristotle",
+  },
+  {
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  {
+    quote:
+      "In order to succeed, your desire for success should be greater than your fear of failure.",
+    author: "Bill Cosby",
+  },
+  {
+    quote: "Winning isn&rsquo;t everything, but wanting to win is.",
+    author: "Vince Lombardi",
+  },
+  {
+    quote: "A person who never made a mistake never tried anything new.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "Life shrinks or expands in proportion to one&rsquo;s courage.",
+    author: "Anais Nin",
+  },
+  {
+    quote:
+      "The most beautiful things in the world cannot be seen or even touched &mdash; they must be felt with the heart.",
+    author: "Helen Keller",
+  },
+  {
+    quote:
+      "Challenges are what make life interesting and overcoming them is what makes life meaningful.",
+    author: "Joshua J. Marine",
+  },
+  {
+    quote: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe",
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    author: "Robert Louis Stevenson",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Whitman",
+  },
+  {
+    quote: "You miss 100% of the shots you don&rsquo;t take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    quote: "An unexamined life is not worth living.",
+    author: "Socrates",
+  },
+  {
+    quote:
+      "When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
+    author: "Helen Keller",
+  },
+];
